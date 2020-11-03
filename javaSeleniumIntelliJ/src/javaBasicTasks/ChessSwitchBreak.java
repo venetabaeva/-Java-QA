b87dff119a8fadd10b75c303c6ba92e7cc295039
venetabaeva <venetabaeva@localhost>
2020-11-03 10:33:43 +0200

    .

--- javaSeleniumIntelliJ/src/javaBasicTasks/ChessSwitchBreak.java
+++ javaSeleniumIntelliJ/src/javaBasicTasks/ChessSwitchBreak.java
@@ -2,45 +2,79 @@ package javaBasicTasks;
 
 import java.util.Scanner;
 
-public class Test {
+public class ChessSwitchBreak {
 	static Scanner sc;
 	static String input;
 
-	public static void  main(String[] args) {
+/*
+ * determines the color of a chessboard square based on its Label and Rank
+ */
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		    
-			sc = new Scanner(System.in);
-		    String l = sc.nextLine();
-		    int r = Integer.parseInt(sc.nextLine());
-		  
-		    
-//		    boolean isOdd = (r %1 ==0);
-//		    boolean isEven = (r %2 == 0);
-		 
-		    		    
 
-		    if (l == "a" || l == "c" ||l == "e" || l == "g" ){
-		    	
-				if (r == 1 || r == 3 || r == 5 || r == 7) {
-					System.out.println("black"); }
-				else {
-					System.out.println("white");
-				}
+		sc = new Scanner(System.in);
+		String l = sc.nextLine();
+		int r = Integer.parseInt(sc.nextLine());
+
+		switch (l) {
+		case "a":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("black");
+			} else {
+				System.out.println("white");
 			}
-		    else {
-		    	 if (l == "b" || l == "d" ||l == "f" || l == "h" ) {
-					System.out.println("white");}
-		    	else {
-					System.out.println("black");
-		    }
-
-
-		    	
-		    	
+			break;
+		case "b":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("white");
+			} else {
+				System.out.println("black");
+			}
+			break;
+		case "c":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("black");
+			} else {
+				System.out.println("white");
+			}
+			break;
+		case "d":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("white");
+			} else {
+				System.out.println("black");
+			}
+			break;
+		case "e":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("black");
+			} else {
+				System.out.println("white");
+			}
+			break;	
+		case "f":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("white");
+			} else {
+				System.out.println("black");
+			}
+			break;
+		case "g":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("black");
+			} else {
+				System.out.println("white");
+			}
+			break;
+		case "h":
+			if (r == 1 || r == 3 || r == 5 || r == 7) {
+				System.out.println("white");
+			} else {
+				System.out.println("black");
+			}
+			break;
 		}
 
 	}
 
 }
-
